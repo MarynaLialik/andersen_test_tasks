f530a1cd7ed2b89dee641f14793439ba786b560c
maryna lialik <maryna.lialik@gmail.com>
2023-02-02 00:10:30 +0100

    feat: add task_2 and task_3 solutions

---
+++ task_2.js
@@ -0,0 +1,28 @@
+function helloForViacheslav() {
+  clearOutput();
+  let value = document.getElementById('entry').value;
+  console.log(`Вы ввели: ${value}`); // optional
+  if (isViacheslav(value)) {
+    showHello('Вячеслав');
+  } else {
+    value.trim() ? showErrorMessage('Нет такого имени.') : showErrorMessage('Введите, пожалуйста, имя.');
+  }
+}
+
+function showHello(value) {
+  console.log(`Привет, ${value}`);
+  document.getElementsByClassName('output')[0].innerHTML = `Привет, ${value}`;
+}
+
+function isViacheslav(value) {
+  return value.toLowerCase() == 'Вячеслав'.toLowerCase();
+}
+
+function clearOutput() {
+  document.getElementsByClassName('output')[0].innerHTML = '';
+}
+
+function showErrorMessage(message) {
+  console.log(message);
+  document.getElementsByClassName('output')[0].innerHTML = message;
+}
